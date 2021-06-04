969ca3af5c4dd97ad308a7e75117f993f8b78522
霁华℡ <leichunqin2002@163.com>
2021-06-04 14:51:32 +0800

    Update 模拟登录.py

--- 模拟登录.py
+++ 模拟登录.py
@@ -1,5 +1,3 @@
 from DecryptLogin import login
 lg = login.Login()
-infos_return, session = lg.zt12306(username='name',password='word')
-def cracker(imagepath):
-    return '1,6'
+infos_return, session = lg.zhihu(username='name',password='word')
